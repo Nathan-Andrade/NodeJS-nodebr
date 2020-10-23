33ce7a0b614d51324c9424d4b9c7f586e8b35f9a
Nathan Andrade <nathandulino2003@gmail.com>
2020-10-23 21:03:40 -0200

    add arrayReduce

---
+++ modulo2/arrayReduce/reduce.js
@@ -0,0 +1,33 @@
+const { getPeople } = require('../for-forin/service');
+
+Array.prototype.myReduce = function(callback){
+  let endValue = typeof endValue !== undefined ? endValue : this[0]
+  for(let index = 0; index <= this.length -1; index++){
+    endValue = callback(endValue, this[index], this)
+  }
+  return endValue
+}
+
+async function main(){
+  try{
+    const { results } = await getPeople(`a`)
+    const pesos = results.map(item => parseInt(item.height))
+   // const total = pesos.reduce((before, after) => {
+   //   return before + after
+   // }, 0)
+   const myList = [
+     ['Nathan', 'Andrade'],
+     ['NodeJs', 'Nerd']
+   ]
+   const total = myList.reduce((before, after) => {
+    return before.concat(after)
+   }, [])
+   .join(', ')
+
+    console.log('total', total)
+
+  } catch (error){
+    console.error(`DEU RUIM NO`, error)
+  }
+}
+main()
