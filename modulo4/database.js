68f777ba10ccde7ea4e2b34fa22dec6b38e41d7c
Nathan Andrade <nathandulino2003@gmail.com>
2020-10-29 20:26:12 -0200

    end CRUD

--- modulo4/database.js
+++ modulo4/database.js
@@ -12,11 +12,13 @@ class Database {
     const file = await readFileAsync(this.FILE_NAME, 'utf8')
     return JSON.parse(file.toString())
   }
+
   async writeFile(data){
     await writeFileAsync(this.FILE_NAME, JSON.stringify(data))
 
     return true
   }
+
   async register(hero){
     const data = await this.getFileData()
     const id = hero.id <= 2 ? hero.id : Date.now();
@@ -26,6 +28,7 @@ class Database {
 
     return result;
   }
+
   async list(id){
     const data = await this.getFileData()
     const filteredData = data.filter(item => (id ? (item.id === id) : true))
@@ -46,6 +49,28 @@ class Database {
     
     data.splice(indice, 1)
     return await this.writeFile(data)
+  }
+
+  async update(id, modifications){
+    const data = await this.getFileData()
+    const indice = data.findIndex(item => item.id === parseInt(id))
+
+    if(indice === -1){
+      throw Error('O heroi informado não existe')
+    }
+    const actual = data[indice]
+
+    const updateObject ={
+      ...actual,
+      ...modifications
+    }
+
+    data.splice(indice, 1)
+    
+    return await this.writeFile([
+      ...data,
+      updateObject
+    ])
   }
 }
 
--- modulo4/test.js
+++ modulo4/test.js
@@ -7,9 +7,16 @@ const DEFAULT_REGISTER_ITEM = {
   id: 1
 }
 
+const DEFAULT_UPDATE_ITEM = {
+  name: 'Lanterna Verde',
+  power: 'Energia do anel',
+  id: 2
+}
+
 describe('Suite de manipulação de herois', () => {
   before(async () =>{
     await database.register(DEFAULT_REGISTER_ITEM)
+    await database.register(DEFAULT_UPDATE_ITEM)
   })
 
   it('deve pesquisar um heroi usando arquivos', async () => {
@@ -28,6 +35,21 @@ describe('Suite de manipulação de herois', () => {
   it('deve remover um heroi por id', async () => {
     const expected = true;
     const result = await database.remove(DEFAULT_REGISTER_ITEM.id)
+    deepEqual(result, expected)
+  })
+  it('deve atualizar um herói pelo id', async () => {
+    const expected ={ 
+      ...DEFAULT_UPDATE_ITEM, 
+      name: 'Batman',
+      power: 'Dinheiro' 
+    }
+    const newData = {
+      name: 'Batman',
+      power: 'Dinheiro' 
+    }
+     await database.update(DEFAULT_UPDATE_ITEM.id, newData)
+    const [result] = await database.list(DEFAULT_UPDATE_ITEM.id)
+
     deepEqual(result, expected)
   })
 })
